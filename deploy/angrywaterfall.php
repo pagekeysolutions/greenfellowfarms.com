bebec4f6280aa4a6c4f0e4f3be0d76a1ed000cc3
Stephen Grice <stephengrice@live.com>
2018-02-14 21:53:49 -0500

    Clean up page references. Remove old rabbits page.

--- deploy/angrywaterfall.php
+++
@@ -1,7 +0,0 @@
-<?php
-echo 'Moving to proper directory <br/>';
-echo shell_exec('cd ~/public_html/domains/greenfellowfarms.com');
-echo 'Attempting git pull <br/>';
-echo shell_exec('git pull origin master');
-echo 'Terminating deploy script';
-?>
